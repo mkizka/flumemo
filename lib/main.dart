d6b6233b77ac88b0c83e7220687faf3630d34248
Compeito <com0806peito@icloud.com>
2019-08-10 13:59:25 +0900

    providerへの移行完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
-import './draw.dart';
+import 'package:provider/provider.dart';
+
+import 'models/pen.dart';
+import 'models/note.dart';
+import 'scenes/edit.dart';
+import 'scenes/menu.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Note(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          builder: (BuildContext context) => NoteModel(),
+        ),
+        ChangeNotifierProvider(
+          builder: (BuildContext context) => PenModel(),
+        ),
+      ],
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (BuildContext context) => EditScene(),
+          '/menu': (BuildContext context) => MenuScene()
+        },
       ),
     );
   }
