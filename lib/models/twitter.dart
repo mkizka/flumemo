de88bc0186fd3ba68795c38b0c05ff29ec2958e7
Compeito <com0806peito@icloud.com>
2019-08-14 22:13:38 +0900

    ツイートの試験的な実装とトークンの保存機能追加

--- lib/models/twitter.dart
+++ lib/models/twitter.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:oauth1/oauth1.dart' as oauth1;
 
 class TwitterModel extends ChangeNotifier {
-  String token;
-  String secret;
+  SharedPreferences _prefs;
+
+  String consumerKey;
+  String consumerSecret;
+
+  String get accessToken => _prefs.getString('accessToken') ?? '';
+
+  set accessToken(String value) => _prefs.setString('accessToken', value);
+
+  String get accessSecret => _prefs.getString('accessSecret') ?? '';
+
+  set accessSecret(String value) => _prefs.setString('accessSecret', value);
+
+  TwitterModel() {
+    SharedPreferences.getInstance().then((instance) {
+      _prefs = instance;
+    });
+    DotEnv().load('.env').then((_) {
+      consumerKey = DotEnv().env['TWITTER_KEY'];
+      consumerSecret = DotEnv().env['TWITTER_SECRET'];
+    });
+  }
+
+  bool get isAuthenticated {
+    return accessToken.isNotEmpty && accessSecret.isNotEmpty;
+  }
 
   Future<void> login() async {
-    await DotEnv().load('.env');
-
     var twitterLogin = new TwitterLogin(
-      consumerKey: DotEnv().env['TWITTER_KEY'],
-      consumerSecret: DotEnv().env['TWITTER_SECRET'],
+      consumerKey: consumerKey,
+      consumerSecret: consumerSecret,
     );
 
     final TwitterLoginResult result = await twitterLogin.authorize();
 
     switch (result.status) {
       case TwitterLoginStatus.loggedIn:
-        token = result.session.token;
-        secret = result.session.secret;
+        accessToken = result.session.token;
+        accessSecret = result.session.secret;
         break;
       case TwitterLoginStatus.cancelledByUser:
         print('canceled');
@@ -31,17 +54,15 @@ class TwitterModel extends ChangeNotifier {
     }
   }
 
-  Future<oauth1.Client> getClient() async {
-    await DotEnv().load('.env');
-
+  oauth1.Client get client {
     var consumerCredentials = oauth1.ClientCredentials(
-      DotEnv().env['TWITTER_KEY'],
-      DotEnv().env['TWITTER_SECRET'],
+      consumerKey,
+      consumerSecret,
     );
 
     var accessTokenCredentials = oauth1.Credentials(
-      token,
-      secret,
+      accessToken,
+      accessSecret,
     );
 
     return oauth1.Client(
@@ -49,5 +70,12 @@ class TwitterModel extends ChangeNotifier {
       consumerCredentials,
       accessTokenCredentials,
     );
+  }
+
+  void tweet() {
+    client.post(
+        'https://api.twitter.com/1.1/statuses/update.json?status=これはDartからのツイート').then((result) {
+          print(result.statusCode);
+    });
   }
 }
